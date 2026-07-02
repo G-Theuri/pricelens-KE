efc088010db198804b1ff3ced889a332783ca7c2
G-Theuri <t.gichukierastus@gmail.com>
2026-07-02 14:27:18 +0300

    Refactor Jumia and Kilimall data extraction and upsert logic; improve error handling and logging

--- jumia.py
+++ jumia.py
@@ -12,58 +12,76 @@ import json
 logger.add("jumia_scraper.log", enqueue="True")
 supabase = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
 
-async def extract_data(tab, url):
-    logger.info(f"Extracting data from: {url}")
-    listings_data = await tab.evaluate("""
-        JSON.stringify(
-            Array.from(document.querySelectorAll('article a.core:not([data-spon="true"])'))
-            .filter(el => el.querySelector("div.info h3.name") !== null)
-            .map(el => {
-                const priceEl = el.querySelector("div.info div.prc");
-                const imgsrc = el.querySelector("div.img-c img");
-                const name = el.querySelector("div.info h3.name");
-                return {
-                    platform_sku: el.getAttribute("data-gtm-id"),
-                    title_raw: name.innerText,
-                    url: el.href,
-                    img: imgsrc ? imgsrc.src : null,
-                    current_price: priceEl ? parseFloat(priceEl.innerText.replace(/[^0-9.]/g, "")) : null,
-                    platform: 'jumia',
-                };
-            })
-            )
-        """)
-    
-    if listings_data:
-        print(json.loads(listings_data))
-        await upsert(json.loads(listings_data))
-
-async def upsert(listings_data):
-    supabase.table('listings').upsert(listings_data, on_conflict="platform,url").execute()
-    
-
 
 async def visit_url(browser, semaphore, url):
-    async with semaphore:
-        tab = await browser.get(url, new_tab=True)
-        await tab.wait(5)
-        await extract_data(tab, url)
-        await tab.close()
-
-
-async def main():
-    browser = await uc.start()
+    try:
+        async with semaphore:
+            tab = await browser.get(url, new_tab=True)
+            try:
+                await tab.wait(5)
+                return await extract_data(tab, url)
+            finally:
+                await tab.close()
+    except Exception as e:
+        logger.info(f"Error getting response from {url} : {e}")
     
-    semaphore = asyncio.Semaphore(10)
-    urls  = [f'https://www.jumia.co.ke/phones-tablets/?page={p}#catalog-listing' for p in range(1, 3)]
-    tasks = [visit_url(browser, semaphore, url) for url in urls]
-    results = await asyncio.gather(*tasks)
 
-    for url, result in zip(urls, results):
+async def extract_data(tab, url):
+    try:
+        logger.info(f"Extracting data from: {url}")
+        listings_data = await tab.evaluate("""
+            JSON.stringify(
+                Array.from(document.querySelectorAll('article a.core:not([data-spon="true"])'))
+                .filter(el => el.querySelector("div.info h3.name") !== null)
+                .map(el => {
+                    const priceEl = el.querySelector("div.info div.prc");
+                    const imgsrc = el.querySelector("div.img-c img");
+                    const name = el.querySelector("div.info h3.name");
+                    return {
+                        platform_sku: el.getAttribute("data-gtm-id"),
+                        title_raw: name.innerText,
+                        url: el.href,
+                        img: imgsrc ? imgsrc.src : null,
+                        current_price: priceEl ? parseFloat(priceEl.innerText.replace(/[^0-9.]/g, "")) : null,
+                        platform: 'jumia',
+                    };
+                })
+                )
+            """)
+
+        return listings_data
+    except Exception as e:
+        logger.error(f"Error extracting data: {e}")
+
+async def upsert(listings_data):
+    try:
+        response = supabase.table('listings').upsert(listings_data, on_conflict="platform,url").execute()
+        return response
+    except Exception as e:
+        logger.error(f"Error upserting to supabase: {e}")
+        return None
+    
+async def main():
+    MAX_CONCURENT = 10
+    listings = []
+    
+    browser = await uc.start()
+    urls  = [f'https://www.jumia.co.ke/phones-tablets/?page={p}#catalog-listing' for p in range(1, 10)]
+
+    logger.info(f"Scraping {len(urls)} pages")
+    semaphore = asyncio.Semaphore(MAX_CONCURENT)
+    logger.info(f"Limiting to {MAX_CONCURENT} scrapes.")
+
+    tasks = [visit_url(browser, semaphore, url) for url in urls]
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    
+    for result in results:
         if isinstance(result, Exception):
-            logger.error(f"FAILED {url}: {result}")
+            logger.warning(f"Scraping page failed {result}")
         else:
-            logger.info(f"OK {url}: {result} listings upserted")
+            response = await upsert(listings)
+            logger.info(f"Upserted {len(response.data)} rows")
+
 
 
 if __name__ == "__main__":
--- kilimall.py
+++ kilimall.py
@@ -5,33 +5,45 @@ from supabase import create_client
 from dotenv import load_dotenv
 load_dotenv()
 import os
+from loguru import logger
 
-
+logger.add("kilimall_scraper.log", enqueue = "True")
 supabase = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
 
 def visit_url():
-    response =  cureq.get("https://mall-api.kilimall.com/search?skip=0&size=50&frontCategoryId=872&useSuggestion=true&limit=36",
-                        impersonate='chrome')
-    return response
+    try:
+        response =  cureq.get("https://mall-api.kilimall.com/search?skip=0&size=50&frontCategoryId=872&useSuggestion=true&limit=36",
+                            impersonate='chrome')
+        return response
+    except Exception as e:
+        logger.error(f"Failed to get response: {e}")
 def extract_data(response):
-    listings = response.json().get("data").get("result")
-    data = []
-    for listing in listings:
-        info = {
+    try:
+        logger.info("Extracting listings data ...")
+        listings = response.json().get("data").get("result")
+        data = []
+        for listing in listings:
+            info = {
 
-            "platform_sku": listing.get("defaultSkuId"),
-            "title_raw": listing.get("shortTitle") or listing.get("title"),
-            "url": "https://www.kilimall.co.ke/listing/" + str(listing.get("listingId")),
-            "img": listing.get("image"),
-            "current_price": listing.get("price"),
-    }
-        info['platform'] = 'kilimall'
-        data.append(info)
+                "platform_sku": listing.get("defaultSkuId"),
+                "title_raw": listing.get("shortTitle") or listing.get("title"),
+                "url": "https://www.kilimall.co.ke/listing/" + str(listing.get("listingId")),
+                "img": listing.get("image"),
+                "current_price": listing.get("price"),
+        }
+            info['platform'] = 'kilimall'
+            data.append(info)
 
-    return data
+        return data
+    except Exception as e:
+        logger.error(f"Failed to extract data: {e}")
 def upsert(data):
-    for info in data:
-        supabase.table("listings").upsert(info, on_conflict="platform, url").execute()
+    try:
+        logger.info("Upserting data into supabase ...")
+        for info in data:
+            supabase.table("listings").upsert(info, on_conflict="platform, url").execute()
+    except Exception as e:
+        logger.error(f"Error upserting data into supabase: {e}")
 
 def main():
     response = visit_url()
